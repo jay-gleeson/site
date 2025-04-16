3b0f196f6c966e0f3ceec9175afe914212873e43
suspendedbelief1iii <jayglee11@gmail.com>
2025-04-15 23:47:56 -0700

    First create-next-app

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+export const content = [
+    './pages/**/*.{js,ts,jsx,tsx}', // Match all pages
+    './components/**/*.{js,ts,jsx,tsx}', // Match all components
+];
+export const theme = {
+    extend: {},
+};
+export const plugins = [];
+  
