d73b571a4ce7c14cdc588aea8f7da2936642aacb
suspendedbelief1iii <jayglee11@gmail.com>
2025-05-25 01:53:10 -0700

    Unnecessary import removal

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@
 import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { JSX } from "react";
 
 // Main page profile tab component
 const ProfileTab = () => (
